841b80d80bea25619c9ba207917faaaa05c35e39
yuki.goto <gtyk1018@gmail.com>
2019-05-23 05:14:24 +0000

    signinlogin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    
+    private
+    
+    def require_user_logged_in
+        unless logged_in?
+            redirect_to login_url
+            flash[:danger] = '正しい動作が行われませんでした。'
+        end
+    end
+    
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
     include SessionsHelper
     before_action :set_tasks, only: [:show, :edit, :update, :destroy]
+    before_action :require_user_logged_in, only: [:index, :show, :edit]
     
     def index
         if logged_in?
